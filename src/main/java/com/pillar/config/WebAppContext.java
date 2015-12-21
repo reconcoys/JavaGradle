cd452e824663d5860e5157a6e28ff0ca56905a5d
Connor <csweeney@pillartechnology.com>
2015-12-21 14:17:08 -0500

    Made a basic gradle web app - run with appStart

---
+++ src/main/java/com/pillar/config/WebAppContext.java
@@ -0,0 +1,34 @@
+package com.pillar.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@ComponentScan("com.pillar.controller")
+@EnableWebMvc
+public class WebAppContext extends WebMvcConfigurerAdapter {
+
+    private static final String JSP_FILE_PATH = "/WEB-INF/";
+    private static final String JSP_FILE_SUFFIX = ".jsp";
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix(JSP_FILE_PATH);
+        viewResolver.setSuffix(JSP_FILE_SUFFIX);
+        return viewResolver;
+    }
+}
---
+++ src/main/java/com/pillar/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.pillar.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String renderHelloView() {
+        return "index";
+    }
+}
