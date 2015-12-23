6c595c95f1abf80c49442e612731cf920a96175c
Connor <csweeney@pillartechnology.com>
2015-12-23 13:58:07 -0500

    failed test to prove jenkins connectedness

--- src/test/java/com/pillar/TestHelloWorld.java
+++ src/test/java/com/pillar/TestHelloWorld.java
@@ -14,4 +14,34 @@ public class TestHelloWorld {
         Assert.assertEquals("pillarTST", pillarTest);
     }
 
+    @Test
+    public void pillarWorld1Test() {
+        String pillarTest = pillarWorld.pillar("pillar");
+        Assert.assertEquals("pillarTEST", pillarTest);
+    }
+
+    @Test
+    public void pillarWorld2Test() {
+        String pillarTest = pillarWorld.pillar("pillar");
+        Assert.assertEquals("pillarTST", pillarTest);
+    }
+
+    @Test
+    public void pillarWorld3Test() {
+        String pillarTest = pillarWorld.pillar("pillar");
+        Assert.assertEquals("pillarTEST", pillarTest);
+    }
+
+    @Test
+    public void pillarWorld4Test() {
+        String pillarTest = pillarWorld.pillar("pillar");
+        Assert.assertEquals("pillarTST", pillarTest);
+    }
+
+    @Test
+    public void pillarWorld5Test() {
+        String pillarTest = pillarWorld.pillar("pillar");
+        Assert.assertEquals("pillarTEST", pillarTest);
+    }
+
 }
